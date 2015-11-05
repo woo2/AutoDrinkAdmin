18a2ee8cd3bb13258e6e1732bfd15ba3ff35e821
Paul Ugolini <paul@paulugolini.com>
2015-11-05 10:08:49 -0500

    Added comments and usage for launcher. Prints prettier info on statup

--- launcher.sh
+++ launcher.sh
@@ -1,14 +1,29 @@
+# AutoDrinkAdmin launcher script
+# launcher.sh
+#
+# Launcher should be run from whatever rc script is being used
+# to start ada on boot.
+#
+# Paul Ugolini <github.com/woo2>
+# Marc Billow <github.com/mbillow> (Original Script)
+
 #!/bin/bash
 
+# if test is present, remove it. It is from the last run. 
+
+# Enter test mode.
 echo "test" > test
 
+# cd to the ADA directory, since we may be operating in a different dir
+cd /home/pi/AutoDrinkAdmin/
+
+echo "Starting AutoDrinkAdmin GUI:"
+echo  "-----------"
 git pull
-sleep 1
+echo  "----------"
+# Show the current version. git log only shows last commit (-1)
+git log -1
+echo -e "-----------\n"
 
-echo -e "Starting AutoDrinkAdmin GUI: \n"
-
-git log --pretty=oneline -1 | fold -w 80 -s
-
-echo "test" > test
-
+# Run the python GUI script
 python gui.py
